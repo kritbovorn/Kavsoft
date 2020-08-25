a48db990846e106e29aa318f7c47a43e80a379c4
kritbovorn <ktaweeyossak@yahoo.com>
2020-08-25 12:44:23 +0700

    MapPreciseLocations Project

---
+++ MapPreciseLocations/MapPreciseLocations/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MapPreciseLocations
+//
+//  Created by Kritbovorn Taweeyossak on 25/8/2563 BE.
+//
+// https://www.youtube.com/watch?v=eDUErLFFgNo
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapPreciseLocations/MapPreciseLocations/MapPreciseLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPreciseLocationsApp.swift
+//  MapPreciseLocations
+//
+//  Created by Kritbovorn Taweeyossak on 25/8/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct MapPreciseLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
