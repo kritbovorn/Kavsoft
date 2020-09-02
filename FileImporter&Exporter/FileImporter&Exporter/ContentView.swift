68e5d03f984ab95b8e3c67366c0190465392b65b
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-02 20:18:10 +0700

    Create FileImporter&Exporter Project

---
+++ FileImporter&Exporter/FileImporter&Exporter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FileImporter&Exporter
+//
+//  Created by Kritbovorn Taweeyossak on 2/9/2563 BE.
+//
+//  https://www.youtube.com/watch?reload=9&v=tnOVnwbkAA0&pp=wgIECgIIAQ%3D%3D&feature=push-fr&attr_tag=eW-qkgmh6OdmAWqo:6
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FileImporter&Exporter/FileImporter&Exporter/FileImporter_ExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileImporter_ExporterApp.swift
+//  FileImporter&Exporter
+//
+//  Created by Kritbovorn Taweeyossak on 2/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct FileImporter_ExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
