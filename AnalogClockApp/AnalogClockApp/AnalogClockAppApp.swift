756a667266b8199a399d6836d7aef652e873b59f
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-09 11:18:13 +0700

    Create AnalogClockApp Project

---
+++ AnalogClockApp/AnalogClockApp/AnalogClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockAppApp.swift
+//  AnalogClockApp
+//
+//  Created by Kritbovorn Taweeyossak on 9/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalogClockApp/AnalogClockApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AnalogClockApp
+//
+//  Created by Kritbovorn Taweeyossak on 9/9/2563 BE.
+//
+//  https://www.youtube.com/watch?v=BTtERko7j1Y&feature=push-prem-sub&attr_tag=u1Omrl9uG2xCQ_Um%3A6
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
