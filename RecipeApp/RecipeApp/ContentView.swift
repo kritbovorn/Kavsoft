a11b1792d5a1d2f22fa0b1a81653fae65a1baf6c
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-20 12:46:10 +0700

    Create Recipe App Project

---
+++ RecipeApp/RecipeApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by Kritbovorn Taweeyossak on 20/9/2563 BE.
+//
+//  https://www.youtube.com/watch?v=MORBfE1RQyY&feature=push-prem-sub&attr_tag=JQZFaFTCusoHqZZp%3A6
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Kritbovorn Taweeyossak on 20/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
