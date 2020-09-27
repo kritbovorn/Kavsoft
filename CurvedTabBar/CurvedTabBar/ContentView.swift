52e0980204f900f6283a7bdd78a6da34066985f1
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-27 19:15:49 +0700

    Create CurvedTabBar Project

---
+++ CurvedTabBar/CurvedTabBar/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CurvedTabBar
+//
+//  Created by Kritbovorn Taweeyossak on 27/9/2563 BE.
+//
+//  https://www.youtube.com/watch?v=6W2vuwWTAU4&list=PLimqJDzPI-H9h7HS40v-5QdwxoC4jrXQu
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurvedTabBar/CurvedTabBar/CurvedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurvedTabBarApp.swift
+//  CurvedTabBar
+//
+//  Created by Kritbovorn Taweeyossak on 27/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct CurvedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
