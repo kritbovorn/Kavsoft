99e6bc63aced0185363ad81cc28073bcbc864fc9
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-29 11:13:43 +0700

    Create SegmentedTabControl Project

---
+++ SegmentedTabControl/SegmentedTabControl/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SegmentedTabControl
+//
+//  Created by Kritbovorn Taweeyossak on 29/9/2563 BE.
+//
+//  https://www.youtube.com/watch?v=KdEqmYpQo6s
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SegmentedTabControl/SegmentedTabControl/SegmentedTabControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedTabControlApp.swift
+//  SegmentedTabControl
+//
+//  Created by Kritbovorn Taweeyossak on 29/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedTabControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
